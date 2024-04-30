447dcd4e8b238a6418a7094b13f4ef23f01527e8
kenneth06123@gmail.com <kenneth06123@gmail.com>
2024-04-30 12:46:29 +0800

    0430/1

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from listings.models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','title','is_published')
+    list_display_links = ('id','title')
+    list_editable = ('is_published',)
+    search_fields = ('title','description','address','city','price')
+    list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)
+
 # Register your models here.
