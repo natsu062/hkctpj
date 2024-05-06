0837a7685d7a6078d402f568d9a0580a62a5f165
kenneth06123@gmail.com <kenneth06123@gmail.com>
2024-05-06 21:35:22 +0800

    0506

--- listings/views.py
+++ listings/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import render
-from .models import Listing
-from django.core.paginator import EmptyPage, PageNotAnInteger,Paginator
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Listing 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def index(request):
     listings = Listing.objects.all().order_by('-list_date').filter(is_published=True) 
-    paginator = Paginator(listings,3)
+    paginator = Paginator(listings, 3)
     page = request.GET.get('page')
     page_listings = paginator.get_page(page)
     context = {'listings': page_listings}
     return render(request, "listings/listings.html", context)
 
 def listing(request, listing_id):
-    return render(request,"listings/listings.html")
+    try:
+        listing = get_object_or_404(Listing,pk=listing_id)
+        context = {
+            'listing':listing
+        }
+        return render(request,"listings/listing.html", context)
+    except Http404:
+        return render(request, '404.html')
 
 def search(request):
     return render(request,"listings/search.html")
